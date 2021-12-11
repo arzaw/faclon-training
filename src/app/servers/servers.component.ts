2fb0687df85e61e1ddbb92d40ea847a119c756ac
arzaw <arzaw_11815115@nitkkr.ac.in>
2021-12-11 21:27:50 +0530

    event binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer=false;
+  serverCreationStatus="no Server found";
 
-  constructor() { }
+  constructor() { 
+
+    setTimeout(()=>{
+      this.allowNewServer=true;
+    }, 2000)
+  }
 
   ngOnInit(): void {
   }
 
+  onServerCreation(){
+    this.serverCreationStatus="Server Created"
+  }
 }
